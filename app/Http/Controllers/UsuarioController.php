f8ee93a9e33bef6cfe2bf9fa805d59a9e5ba7d6a
LeidyFonseca24 <66701537+LeidyFonseca24@users.noreply.github.com>
2022-11-22 18:36:34 -0500

    PDF

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class UsuarioController extends Controller
+{
+
+    public function index()
+    {
+        $usuario = User::get();
+        return view('auth.usuario.index', compact('usuario'));
+    }
+
+    public function create()
+    {
+        $role = Role::All();
+        return view('auth.usuario.create')->with('role',$role);
+    }
+
+    public function store(Request $request)
+    {
+
+        $datos = $request->validate(
+            [
+                'name' => 'required',
+                'email' => 'required|max:50| email',
+                'id_Role_FK' => 'required',
+                'password' => 'required|max:60'
+            ]
+        );
+        $password=Str::random(20);
+        $datos["password"]=$password;
+        $usuario = User::create($datos);
+        return view('auth.usuario.creado');
+    }
+
+    public function show(User $usuario)
+    {
+        //
+    }
+
+    public function edit(User $usuario)
+    {
+        $role = Role::All();
+        //var_dump($usuario);
+        return view('PerfilAdmin', compact('usuario'))->with('usuario',$usuario);
+    }
+
+    public function update(Request $request, User $usuario)
+    {
+        $datos = $request->validate(
+            [
+                'name' => 'required',
+                'email' => 'required|max:50| email',
+                'id_Role_FK',
+                'password' => 'required|max:60'
+            ]
+        );
+        $usuario->update($datos);
+        return view('auth.usuario.actualizado');
+    }
+
+    public function destroy(User $usuario)
+    {
+        $usuario->delete();
+        return view('auth.usuario.borrado');
+    }
+}
